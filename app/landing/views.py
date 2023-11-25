a597c35ecc64e630625bb84d412ce32a8c52da95
nima <54316106+nimadorostkar@users.noreply.github.com>
2023-11-25 11:24:46 +0330

    Update views.py

--- app/landing/views.py
+++ app/landing/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from itertools import chain
+
 from rest_framework import viewsets, filters, status, pagination, mixins
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
