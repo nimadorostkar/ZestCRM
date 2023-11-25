81fb9469ba57e06d2b86176e292bac82c0889b9b
nima <54316106+nimadorostkar@users.noreply.github.com>
2023-11-25 11:24:56 +0330

    Update admin.py

--- app/branch/admin.py
+++ app/branch/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Branch
 
+
 class BranchAdmin(admin.ModelAdmin):
     list_display = ('name', 'branch_manager', 'branch_seller', 'city', 'created_on')
     raw_id_fields = ('branch_manager'),
